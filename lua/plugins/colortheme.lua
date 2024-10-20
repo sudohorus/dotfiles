37ae688399b0535bdbe0e822f26781f087eea6aa
sudohorus <sudohorusdev@gmail.com>
2024-10-19 22:16:32 -0300

    gruber-darker theme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,26 +1,10 @@
-return {
-    'shaunsingh/nord.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.nord_contrast = true
-        vim.g.nord_borders = false
-        vim.g.nord_disable_background = false
-        vim.g.nord_italic = false
-        vim.g.nord_uniform_diff_background = true
-        vim.g.nord_bold = false
-
-        require('nord').set()
-
-        local bg_transparent = true
-
-        local toggle_transparency = function()
-            bg_transparent = not bg_transparent
-            vim.g.nord_disable_background = bg_transparent
-            vim.cmd [[colorscheme nord]]
-        end
-
-        vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
-    end
+return{
+  'thimc/gruber-darker.nvim',
+  config = function()
+    require('gruber-darker').setup({
+      transparent = false, 
+      bold = false
+    })
+    vim.cmd.colorscheme('gruber-darker')
+  end,
 }
-
