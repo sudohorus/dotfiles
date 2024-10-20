61b456b6984fa698346dd38b5c3bd280b19203f6
sudohorus <sudohorusdev@gmail.com>
2024-10-19 22:05:59 -0300

    idk

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ require 'core.keymaps'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
 end vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
@@ -17,7 +17,7 @@ require('lazy').setup({
     require 'plugins.lualine',
     require 'plugins.treesitter',
     require 'plugins.telescope',
-    require 'plugins.autocompletion',
+    require 'plugins.autocompletion',   
 
     { 'wakatime/vim-wakatime', lazy = false }
 })
