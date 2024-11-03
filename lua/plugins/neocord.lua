cc20360264e2768249884841ed9d7721bbd21aa5
sudohorus <sudohorusdev@gmail.com>
2024-11-03 16:03:24 -0300

    discord rich presence update

--- lua/plugins/neocord.lua
+++ lua/plugins/neocord.lua
@@ -3,8 +3,8 @@ return {
     config = function()
         require("presence").setup({
             auto_update        = true,
-            neovim_image_text  = "Neovim IDE",
-            main_image         = "file",
+            neovim_image_text  = "The One True Text Editor",
+            main_image         = "neovim",
             client_id          = "1299830136178475069",
             log_level          = nil,
             debounce_timeout   = 10,
